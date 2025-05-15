a1459f8c163dca2409ab8e00d75d1363f714b785
gunflake <gunflake09@naver.com>
2025-05-15 21:11:36 +0900

    feat: MongoDB 연결을 위한 DatabaseModule 구성 및 옵션 인터페이스 추가

--- libs/database/src/lib/database.module.ts
+++ libs/database/src/lib/database.module.ts
@@ -1,8 +1,30 @@
-import { Module } from '@nestjs/common';
+// libs/database/src/lib/database.module.ts
+import { DynamicModule, Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
-@Module({
-  controllers: [],
-  providers: [],
-  exports: [],
-})
-export class DatabaseModule {}
+// MongoDB 연결 옵션을 위한 인터페이스 정의
+export interface DatabaseOptions {
+  dbName?: string;
+  appName?: string;
+  retryWrites?: boolean;
+  [key: string]: any; // 기타 mongoose 옵션들을 위한 설정
+}
+
+@Module({})
+export class DatabaseModule {
+  static forRoot(uri: string, options: DatabaseOptions = {}): DynamicModule {
+    return {
+      module: DatabaseModule,
+      imports: [
+        MongooseModule.forRoot(uri, {
+          dbName: options.dbName,
+          appName: options.appName,
+          retryWrites:
+            options.retryWrites !== undefined ? options.retryWrites : true,
+          ...options,
+        }),
+      ],
+      global: true,
+    };
+  }
+}
