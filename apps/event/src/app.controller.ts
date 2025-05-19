cec900221b404676205f33f2bcb010ec549c9cb0
gunflake <gunflake09@naver.com>
2025-05-19 18:36:24 +0900

    refactor: app module 파일 구조 변경

--- apps/event/src/app.controller.ts
+++ apps/event/src/app.controller.ts
@@ -1,5 +1,4 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
@@ -7,6 +6,6 @@ export class AppController {
 
   @Get()
   getData() {
-    return this.appService.getData();
+    return { message: 'Hello event API' };
   }
 }
